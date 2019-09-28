067919c71ebc6291db5c09b97432609a5398542c
bhautikg <52224896+bhautikg@users.noreply.github.com>
2019-09-27 18:11:13 -0700

    cleaned up folders and duplicates
    resolved conflict

--- src/src/main/java/com/insight/HostURLs.java
+++
@@ -1,8 +0,0 @@
-package com.insight;
-
-//https://github.com/kaenyyh/Insight_project_2018b/blob/master/processing/streaming/src/main/java/com/insight/HostURLs.java
-public class HostURLs {
-    public String ZOOKEEPER_URL     = "ec2-54-191-198-59.us-west-2.compute.amazonaws.com";
-    public String KAFKA_URL         = "ec2-54-191-198-59.us-west-2.compute.amazonaws.com";
-    public String Flink_URL 	    = "ec2-54-245-60-184.us-west-2.compute.amazonaws.com";
-}
