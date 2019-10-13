c25a641a75c522313a1629e5181e42f187469e76
bhautikg <52224896+bhautikg@users.noreply.github.com>
2019-10-13 15:52:03 -0700

    Update runflink.sh
    Had the wrong bash script in Runflink, corrected it

--- src/flink/runflink.sh
+++ src/flink/runflink.sh
@@ -1,5 +1,4 @@
-IP_ADDR="$(hostname)"
-DEPS="../../python/helpers.py,../spark_helpers.py,../spark_consumer.py"
-DEPS_ARR=($DEPS)
-
-/usr/local/spark/bin/spark-submit --master spark://$IP_ADDR:7077 --num-executors 3 --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 --py-files $DEPS  main_ecg_consumer.py
+cd ~/InsightHeartbeat/src/Flink
+mvn clean package
+cd /usr/local/flink
+./bin/flink run ~/InsightHeartbeat/src/Flink/target/flink-process-v1.jar
