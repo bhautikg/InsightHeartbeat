b35eafaf14f06d287109adaa33ad0861a56391d4
bhautikg <52224896+bhautikg@users.noreply.github.com>
2019-10-13 15:49:24 -0700

    Delete runflink.sh
    in the wrong folder, correcting the folder

--- src/Flink/runflink.sh
+++
@@ -1,5 +0,0 @@
-IP_ADDR="$(hostname)"
-DEPS="../../python/helpers.py,../spark_helpers.py,../spark_consumer.py"
-DEPS_ARR=($DEPS)
-
-/usr/local/spark/bin/spark-submit --master spark://$IP_ADDR:7077 --num-executors 3 --packages org.apache.spark/spark-streaming-kafka-0-8_2.11/2.3.1 --py-files $DEPS  main_ecg_consumer.py
